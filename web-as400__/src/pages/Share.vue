38bab8f150f083a485feddeb124289c374c4f2bd
tommal <tommal@iMac-di-tommal.local>
2023-03-29 23:59:08 +0200

    fixed position

--- web-as400__/src/pages/Share.vue
+++ web-as400__/src/pages/Share.vue
@@ -2,18 +2,19 @@
 
 
 <template>
-    <iframe src="https://tomasmali.it/upload" width="100%" height="900">
+    <iframe src="https://tomasmali.it/upload" width="100%" height="850">
     </iframe>
     <div class="text-center">
-        <h5>
+        <h6>
             Quest tool permette di condividere tra qualsiasi PC senza bisogno di VPN file piccoli o di grandi dimensioni
             (fino a 10 GB)
             Basta solo caricare il file qui sopra e condividere il link con l'altro PC.
-        </h5>
-        <h4>
-            Per condividere esternamente i file caricati, vai a <a href="https://tomasmali.it/upload" target="_blank">
-                questa</a> pagina dall'altro PC e clicca sul file che hai caricato sopra per ottenerlo.
-        </h4>
+      <div>
+        
+          Per condividere esternamente i file caricati, vai a <a href="https://tomasmali.it/upload" target="_blank">
+              questa</a> pagina dall'altro PC e clicca sul file che hai caricato sopra per ottenerlo.
+      </div>
+        </h6>
     </div>
 
 </template>
